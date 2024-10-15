7588dda833c9c95cea477be13f89e71593365cdd
liu-lzcer <91863749+liu-lzcer@users.noreply.github.com>
2024-10-15 14:43:15 +0800

    框架搭建

--- ForumProject/src/main/common/forumsystem/model/Post.java
+++ ForumProject/src/main/common/forumsystem/model/Post.java
@@ -1,9 +1,36 @@
 package common.forumsystem.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Post {
     private String title;
     private String content;
     private User author;
+    private List<String> replies;
 
+    public Post (String title, String content, User author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.replies = new ArrayList<>();
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
 
+    public User getAuthor() {
+        return author;
+    }
+
+    public List<String> getReplies() {
+        return replies;
+    }
+
+    public void addReplies(String reply) {
+        replies.add(reply);
+    }
 }
--- ForumProject/src/main/common/forumsystem/service/PostManager.java
+++ ForumProject/src/main/common/forumsystem/service/PostManager.java
@@ -1,4 +1,5 @@
 package common.forumsystem.service;
 
 public class PostManager {
+
 }
